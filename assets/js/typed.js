b4084f92801a1e5bef619a3036d5f1dc9b8ec009
ACURSEDPRINCE <123055628+ACURSEDPRINCE@users.noreply.github.com>
2023-02-25 13:54:17 -0500

    test

---
+++ assets/js/typed.js
@@ -0,0 +1,7 @@
+new Typed('#typed',{
+  strings : [,'.gg/5fov','ACURSEDPRINCE','torsoimortal fan','THEBIGGGGWALLUS',],
+  typeSpeed : 40,
+  delaySpeed : 90,
+  showCursor: false,
+  loop : true
+})

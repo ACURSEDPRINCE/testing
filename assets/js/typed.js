c2fc7206a736b17b243e3f2a79e460d759d143d5
ACURSEDPRINCE <123055628+ACURSEDPRINCE@users.noreply.github.com>
2023-02-26 22:01:23 -0500

    Delete assets directory

--- assets/js/typed.js
+++
@@ -1,7 +0,0 @@
-new Typed('#typed',{
-  strings : [,'.gg/5fov','ACURSEDPRINCE','torsoimortal fan','THEBIGGGGWALLUS',],
-  typeSpeed : 40,
-  delaySpeed : 90,
-  showCursor: false,
-  loop : true
-})
